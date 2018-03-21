dbb3666d463d6a1cce08b2f97a0c2340aae6ad4d
luyu <luyu_6666@qq.com>
2018-03-21 15:53:59 +0800

    添加了 Park Notify BatchNotify

--- io/ekt8/tx_pool/tx_pool.go
+++ io/ekt8/tx_pool/tx_pool.go
@@ -1,6 +1,9 @@
 package tx_pool
 
-import "github.com/EducationEKT/EKT/io/ekt8/core/common"
+import (
+	"github.com/EducationEKT/EKT/io/ekt8/core/common"
+	"sort"
+)
 
 const (
 	Block = iota
@@ -9,21 +12,54 @@ const (
 
 var txPool TxPool
 
+type BlockQueue [] *common.Transaction
+
 func init() {
 	txPool = TxPool{}
 }
 
 type TxPool struct {
-	txs map[string]*common.Transaction
+	ready map[string]*common.Transaction
+	wait BlockQueue
 }
 
 func GetTxPool() TxPool {
 	return txPool
 }
 
-func (txPool TxPool) Park(tx *common.Transaction) {
-	if txPool.txs[tx.TransactionId] != nil {
-		return
-	}
-	txPool.txs[tx.TransactionId] = tx
+//func (txPool TxPool) Park(tx *common.Transaction) {
+//	if txPool.ready[tx.TransactionId] != nil {
+//		return
+//	}
+//	txPool.ready[tx.TransactionId] = tx
+//}
+
+func (txPool TxPool) Park(tx *common.Transaction ,reason int){
+
+}
+
+func (TxPool TxPool) Notify(tx *common.Transaction ){
+
+}
+
+func (txPool TxPool) BatchNotify(txs []*common.Transaction){
+
+}
+
+
+func (blockQueue BlockQueue) Len() int{
+	return len(blockQueue)
+}
+
+func (blockQueue BlockQueue) Swap(i, j int){
+	blockQueue[i].Nonce, blockQueue[j].Nonce =blockQueue[j].Nonce, blockQueue[i].Nonce
+}
+
+func (blockQueue BlockQueue) Less(i, j int) bool {
+	return blockQueue[i].Nonce < blockQueue[j].Nonce
+}
+
+//just test sort function
+func test(){
+	sort.Sort(txPool.wait)
 }
