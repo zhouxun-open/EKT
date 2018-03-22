c794f04f3a58e1906c6589143fe8851fd9cf8cac
luyu <luyu_6666@qq.com>
2018-03-22 14:53:44 +0800

    test UserTransactions sort

--- io/ekt8/tx_pool/tx_pool_test.go
+++ io/ekt8/tx_pool/tx_pool_test.go
@@ -2,10 +2,13 @@ package tx_pool
 
 import (
 	"testing"
+	"github.com/EducationEKT/EKT/io/ekt8/core/common"
+	"sort"
+	"fmt"
 )
 
 func TestTxPool_Fetch(t *testing.T) {
-	println(txPool.Fetch(0))
+	//println(txPool.Fetch(0))
 	txPool.Fetch(1)
 	size := len(txPool.ready)
 	txPool.Fetch(size)
@@ -16,4 +19,17 @@ func BenchmarkTxPool_Fetch(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		txPool.Fetch(1)
 	}
+}
+
+func TestUserTransactions_sort(t *testing.T) {
+	t1:=common.Transaction{Nonce:1}
+	t2:=common.Transaction{Nonce:2}
+	t3:=common.Transaction{Nonce:3}
+	u:=UserTransactions{}
+	u=append(u,&t3,&t2,&t1)
+	fmt.Println("before sort")
+	fmt.Println(u)
+	fmt.Println("after sort")
+	sort.Sort(u)
+	fmt.Println(u)
 }
