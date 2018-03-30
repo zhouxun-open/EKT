dc51d7b8ce21b346d351fe82042021c81ece309c
luyu <luyu_6666@qq.com>
2018-03-30 15:20:11 +0800

    add a func to receive something

--- io/ekt8/api/consenus.go
+++ io/ekt8/api/consenus.go
@@ -1,4 +1,20 @@
 package api
+
+import (
+	"github.com/EducationEKT/xserver/x_http/x_req"
+	"github.com/EducationEKT/xserver/x_http/x_resp"
+	"github.com/EducationEKT/xserver/x_err"
+	"fmt"
+	"github.com/EducationEKT/xserver/x_http/x_router"
+)
+
 /*
 用来与其他区块进行基于共识机制的通信
 */
+func init(){
+	x_router.Post("/consenus/api/receive",receive)
+}
+
+func receive(req *x_req.XReq)(*x_resp.XRespContainer, *x_err.XErr){
+	return x_resp.Success("receive"),nil
+}
