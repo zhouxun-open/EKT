f064f6dcfb32b40272689a5bb910a1be190cb952
zhouxun <806275025@qq.com>
2018-04-16 21:49:30 +0800

    add benchmark

--- io/ekt8/crypto/crypto_test.go
+++ io/ekt8/crypto/crypto_test.go
@@ -1,24 +1,60 @@
 package crypto
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"testing"
-
-	"github.com/ethereum/go-ethereum/crypto/randentropy"
 )
 
 func TestGenerateKeyPair(t *testing.T) {
 	pub, priv := GenerateKeyPair()
 	fmt.Printf("pubKey=%s, secKey=%s\n", hex.EncodeToString(pub), hex.EncodeToString(priv))
-	data := randentropy.GetEntropyCSPRNG(32)
+	data := Sha3_256([]byte("hello world"))
 	sign, err := Crypto(data, priv)
 	if err != nil {
 		fmt.Println(err)
 		t.Fail()
 	}
+	if pubKey, err := RecoverPubKey(data, sign); err == nil && bytes.Equal(pubKey, pub) {
+		fmt.Printf(`recovered public key: %s`, hex.EncodeToString(pubKey))
+	} else {
+		t.Fail()
+	}
 	if !Verify(sign, pub, data) {
 		fmt.Println("verify fail")
 		t.Fail()
+	}
+}
+
+func BenchmarkCrypto(b *testing.B) {
+	_, priv := GenerateKeyPair()
+	for i := 0; i < b.N; i++ {
+		data := Sha3_256([]byte("hello world"))
+		Crypto(data, priv)
+	}
+}
+
+func BenchmarkRecoverPubKey(b *testing.B) {
+	_, priv := GenerateKeyPair()
+	data := Sha3_256([]byte("hello world"))
+	sign, err := Crypto(data, priv)
+	if err != nil {
+		b.Fail()
+	}
+	for i := 0; i < b.N; i++ {
+		RecoverPubKey(data, sign)
+	}
+}
+
+func BenchmarkVerify(b *testing.B) {
+	pub, priv := GenerateKeyPair()
+	data := Sha3_256([]byte("hello world"))
+	sign, err := Crypto(data, priv)
+	if err != nil {
+		b.Fail()
+	}
+	for i := 0; i < b.N; i++ {
+		Verify(sign, pub, data)
 	}
 }
