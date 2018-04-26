bded7125cf92c44a887a674737802e8093d65091
zhouxun <zhouxun@xuexibao.cn>
2018-04-26 14:21:59 +0800

    block record

--- io/ekt8/blockchain/block_record.go
+++ io/ekt8/blockchain/block_record.go
@@ -1,1 +1,26 @@
 package blockchain
+
+import "encoding/hex"
+
+var BlockRecorder *BlockRecord
+
+func init() {
+	BlockRecorder = &BlockRecord{
+		Blocks:     make(map[string]*Block),
+		Signatures: make(map[string]string),
+	}
+}
+
+type BlockRecord struct {
+	Blocks     map[string]*Block
+	Signatures map[string]string
+}
+
+func GetBlockRecordInst() *BlockRecord {
+	return BlockRecorder
+}
+
+func (record BlockRecord) Record(block *Block, sign []byte) {
+	record.Blocks[hex.EncodeToString(block.Hash())] = block
+	record.Signatures[hex.EncodeToString(block.Hash())] = hex.EncodeToString(sign)
+}
