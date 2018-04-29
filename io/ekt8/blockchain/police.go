1d23ba901e8f6513d14a4cec6cfe685e3a4045f5
zhouxun <zhouxun@xuexibao.cn>
2018-04-29 17:40:57 +0800

    修改监督相同节点广播不同区块的逻辑

--- io/ekt8/blockchain/police.go
+++ io/ekt8/blockchain/police.go
@@ -1,8 +1,25 @@
 package blockchain
 
+import "bytes"
+
 type BlockPolice struct {
 	// 记录从其他节点过来的block
-	BlockFromPeer map[int]*Block
-	//
-	Vote map[string]bool
+	PeerBlocks map[int64][]*Block
+}
+
+// 返回-1表示同一个节点打包了不同的区块
+// 返回 0表示已经记录过此区块
+// 返回 1表示未记录过此区块，需要进行投票
+func (police BlockPolice) BlockFromPeer(block *Block) int {
+	if blocks, exist := police.PeerBlocks[block.Height]; exist {
+		for _, _block := range blocks {
+			if !bytes.Equal(block.CurrentHash, _block.CurrentHash) && block.Round.Equal(_block.Round) {
+				return -1
+			}
+		}
+	} else {
+		blocks = append(blocks, block)
+		return 1
+	}
+	return 0
 }
