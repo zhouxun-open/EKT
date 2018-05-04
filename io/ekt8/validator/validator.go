a4bd9d682deacbe3d74e8ec32f28a49f2995498d
EducationEKT <xlgfzh@gmail.com>
2018-05-04 20:21:41 +0800

    删除无用代码

--- io/ekt8/validator/validator.go
+++ io/ekt8/validator/validator.go
@@ -9,18 +9,6 @@ import (
 	"github.com/EducationEKT/EKT/io/ekt8/stat"
 )
 
-func Validate(address, msg, sign []byte) bool {
-	account, err := stat.GetAccount(address)
-	if err != nil {
-		return false
-	}
-	pubkey, err := crypto.RecoverPubKey(msg, sign)
-	if err != nil {
-		return false
-	}
-	return bytes.Equal(pubkey, account.PublicKey())
-}
-
 func ValidateTx(tx *common.Transaction) bool {
 	address, err := hex.DecodeString(tx.From)
 	if err != nil {
