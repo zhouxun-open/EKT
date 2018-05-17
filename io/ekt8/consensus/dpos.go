15e069f29c37596065292efd7798778bed5b47cd
EducationEKT <xlgfzh@gmail.com>
2018-05-17 18:04:04 +0800

    delete unused code

--- io/ekt8/consensus/dpos.go
+++ io/ekt8/consensus/dpos.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"runtime/debug"
 	"sort"
 
 	"xserver/x_http/x_resp"
@@ -84,11 +85,11 @@ func (dpos DPOSConsensus) DPoSRun() {
 	defer dpos.DPOSRunLocker.Unlock()
 	fmt.Println("DPoS running.")
 	for {
-		//defer func() {
-		//	if r := recover(); r != nil {
-		//		fmt.Println("Panic occured.", r)
-		//	}
-		//}()
+		defer func() {
+			if r := recover(); r != nil {
+				debug.PrintStack()
+			}
+		}()
 		time.Sleep(interval)
 		round := &i_consensus.Round{Peers: param.MainChainDPosNode, CurrentIndex: -1}
 		if dpos.Blockchain.CurrentHeight > 0 {
@@ -168,50 +169,6 @@ func (dpos DPOSConsensus) PeerTurn(packTime int64, peer p2p.Peer) bool {
 
 func (dpos DPOSConsensus) IsMyTurn() bool {
 	return dpos.PeerTurn(time.Now().UnixNano()/1e6, conf.EKTConfig.Node)
-	//fmt.Printf("Current block height is %d.\n", dpos.Blockchain.CurrentHeight)
-	//time, interval := int(time.Now().UnixNano()/1e6-dpos.Blockchain.CurrentBlock.Timestamp), int(dpos.Blockchain.BlockInterval/1e6)
-	//// 如果当前时间与上个区块的打包时间超过一个round，需要等待round的下一个节点进行打包
-	//round := &i_consensus.Round{
-	//	Peers:        param.MainChainDPosNode,
-	//	CurrentIndex: -1,
-	//}
-	//if dpos.Blockchain.CurrentHeight > 0 {
-	//	round = dpos.Blockchain.CurrentBlock.Round
-	//} else {
-	//	// 如果是第一个区块，需要第一个节点来打包
-	//	if conf.EKTConfig.Node.Equal(round.Peers[0]) {
-	//		return true
-	//	} else {
-	//		return false
-	//	}
-	//}
-	//if time >= interval*round.Len() {
-	//	// 如果当前节点是下一个节点
-	//	if round.IndexPlus(dpos.Blockchain.CurrentBlock.Hash()).IsMyTurn() {
-	//		return true
-	//	} else {
-	//		return false
-	//	}
-	//}
-	//n := time / interval
-	//remainder := int(time) % int(interval)
-	//if remainder > int(interval)/2 {
-	//	n++
-	//}
-	//fmt.Printf("Current round is %s \n", round.String())
-	//if round.CurrentIndex+n >= round.Len() {
-	//	round = round.NewRandom(dpos.Blockchain.CurrentBlock.CurrentHash)
-	//	fmt.Printf("Next round is %s, is my turn? \n", round.String())
-	//	sort.Sort(round)
-	//}
-	//currentIndex := (round.CurrentIndex + n) % round.Len()
-	//if round.Peers[currentIndex].Equal(conf.EKTConfig.Node) {
-	//	fmt.Println("Yes.")
-	//	return true
-	//} else {
-	//	fmt.Println("No.")
-	//	return false
-	//}
 }
 
 func (dpos DPOSConsensus) RUN() {
