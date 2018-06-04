f146ff09ceeea4955c5c247246c814bce5263a2d
EducationEKT <xlgfzh@gmail.com>
2018-06-04 15:20:40 +0800

    修改block_record的相关逻辑

--- io/ekt8/blockchain/block_record.go
+++ io/ekt8/blockchain/block_record.go
@@ -12,7 +12,7 @@ func init() {
 }
 
 type BlockRecord struct {
-	Status1    sync.Map
+	Locker     sync.RWMutex
 	Status     map[string]int
 	Blocks     map[string]*Block
 	Signatures map[string]string
@@ -20,10 +20,26 @@ type BlockRecord struct {
 
 func NewBlockRecorder() *BlockRecord {
 	return &BlockRecord{
+		Locker:     sync.RWMutex{},
 		Blocks:     make(map[string]*Block),
 		Status:     make(map[string]int),
 		Signatures: make(map[string]string),
 	}
+}
+
+func (recorder BlockRecord) GetStatus(blockHash string) int {
+	recorder.Locker.RLock()
+	defer recorder.Locker.RUnlock()
+	if status, exist := recorder.Status[blockHash]; exist {
+		return status
+	}
+	return -1
+}
+
+func (recorder BlockRecord) SetStatus(blockHash string, status int) {
+	recorder.Locker.Lock()
+	defer recorder.Locker.Unlock()
+	recorder.Status[blockHash] = status
 }
 
 func GetBlockRecordInst() *BlockRecord {
