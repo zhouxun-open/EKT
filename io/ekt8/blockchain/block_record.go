0926db8a80cb043e08de333067f942a32ce16374
EducationEKT <xlgfzh@gmail.com>
2018-06-04 13:26:03 +0800

    修改block_record的数据机构

--- io/ekt8/blockchain/block_record.go
+++ io/ekt8/blockchain/block_record.go
@@ -1,18 +1,29 @@
 package blockchain
 
-import "encoding/hex"
+import (
+	"encoding/hex"
+	"sync"
+)
 
 var BlockRecorder *BlockRecord
 
 func init() {
-	BlockRecorder = &BlockRecord{
-		Blocks: make(map[string]*Block),
-	}
+	BlockRecorder = NewBlockRecorder()
 }
 
 type BlockRecord struct {
+	Status1    sync.Map
+	Status     map[string]int
 	Blocks     map[string]*Block
 	Signatures map[string]string
+}
+
+func NewBlockRecorder() *BlockRecord {
+	return &BlockRecord{
+		Blocks:     make(map[string]*Block),
+		Status:     make(map[string]int),
+		Signatures: make(map[string]string),
+	}
 }
 
 func GetBlockRecordInst() *BlockRecord {
