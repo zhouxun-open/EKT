1132de2343b4add01defd21b613dc3b3bfc02644
EKT <xlgfzh@gmail.com>
2018-06-21 19:06:10 +0800

    新增获取用户信息的api

--- io/ekt8/api/user.go
+++ io/ekt8/api/user.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"encoding/hex"
+	"github.com/EducationEKT/EKT/io/ekt8/blockchain_manager"
+	"github.com/EducationEKT/EKT/io/ekt8/context_log"
 	"github.com/EducationEKT/xserver/x_err"
 	"github.com/EducationEKT/xserver/x_http/x_req"
 	"github.com/EducationEKT/xserver/x_http/x_resp"
@@ -8,9 +11,20 @@ import (
 )
 
 func init() {
-	x_router.Post("/user/api/regist")
+	x_router.Get("/user/api/info", userInfo)
 }
 
-func NewAccount(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
-	return x_resp.Fail(-1, "This function is not open now.", nil), nil
+func userInfo(req *x_req.XReq) (*x_resp.XRespContainer, *x_err.XErr) {
+	address := req.MustGetString("address")
+	log := context_log.NewContextLog("userInfo")
+	defer log.Finish()
+	hexAddress, err := hex.DecodeString(address)
+	if err != nil {
+		x_resp.Fail(-1, "error address", nil)
+	}
+	account, err := blockchain_manager.GetMainChain().GetLastBlock().GetAccount(log, hexAddress)
+	if err != nil {
+		x_resp.Fail(-1, err.Error(), nil)
+	}
+	return x_resp.Return(account, nil)
 }
