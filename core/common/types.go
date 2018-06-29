7703d7208673132b45d46b83f73ebc3ac4c32bd7
EducationEKT <xlgfzh@gmail.com>
2018-06-29 11:23:00 +0800

    修改打包和投票细节

--- core/common/types.go
+++ core/common/types.go
@@ -1,18 +1,3 @@
 package common
 
-import (
-	"encoding/hex"
-	"fmt"
-)
-
 type HexBytes []byte
-
-func (bytes HexBytes) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`"%s"`, hex.EncodeToString(bytes))), nil
-}
-
-type Object interface{}
-
-type CoinType int
-
-type Time int64
