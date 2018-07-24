c98534d13c140f30c78a6cb781e95ab9fb0cffe9
EducationEKT <xlgfzh@gmail.com>
2018-07-24 17:31:17 +0800

    blockBody不再带有高度

--- blockchain/body_test.go
+++ blockchain/body_test.go
@@ -4,24 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
-
-	"github.com/EducationEKT/EKT/userevent"
 )
 
 func TestBlockBody_AddEventResult(t *testing.T) {
-	body := NewBlockBody(1)
-	eventResult := userevent.EventResult{
-		EventId: "123",
-		Success: true,
-		Reason:  "",
-	}
-	body.AddEventResult(eventResult)
-	eventResult2 := userevent.EventResult{
-		EventId: "122",
-		Success: false,
-		Reason:  "err sign",
-	}
-	body.AddEventResult(eventResult2)
+	body := NewBlockBody()
 	bodyData, _ := json.Marshal(body)
 	fmt.Println(string(bodyData))
 }
